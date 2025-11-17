a8be882b2f2c63b23978b592580be1ad3aaac364
Steamcollect <eliot.garnier.080805@gmail.com>
2025-11-17 13:59:32 +0100

    Wip

--- Assets/App/Scripts/Card/CardPlacementManager.cs
+++ Assets/App/Scripts/Card/CardPlacementManager.cs
@@ -1,18 +1,61 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CardPlacementManager : MonoBehaviour
 {
-    //[Header("Settings")]
+    [Header("Settings")]
+    [SerializeField] float gridSize = 1;
+
     //[Header("References")]
+    Camera cam;
+
     Card currentCardHandle;
 
-    //[Header("Input")]
+    [Header("Input")]
+    [SerializeField] InputActionReference mousePosition;
+    [SerializeField] InputActionReference clearCardHandle;
+
     //[Header("Output")]
+
+    private void OnEnable()
+    {
+        clearCardHandle.action.started += ClearCardHandle;
+    }
+    private void OnDisable()
+    {
+        clearCardHandle.action.started -= ClearCardHandle;
+    }
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        MoveCurrentCardHandle();
+    }
+
+    void MoveCurrentCardHandle()
+    {
+        if (!currentCardHandle) return;
+
+        Vector2 mousePos = mousePosition.action.ReadValue<Vector2>();
+        Vector2 gridPos = ((Vector2)Vector2Int.RoundToInt(cam.ScreenToWorldPoint(mousePos))) * gridSize;
+
+        currentCardHandle.transform.position = gridPos;
+    }
 
     public void HandleNewCard(SSO_CardData card)
     {
-        if(currentCardHandle) Destroy(currentCardHandle.gameObject);
+        if (currentCardHandle) Destroy(currentCardHandle.gameObject);
 
         currentCardHandle = Instantiate(card.cardPrefab, transform);
+    }
+
+    void ClearCardHandle(InputAction.CallbackContext context)
+    {
+        if(currentCardHandle) 
+            Destroy(currentCardHandle.gameObject);
     }
 }
