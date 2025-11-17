81ab620fa37b3476f31ba1559ff8aa15de0b8622
RomainUTR <work.romainutr@gmail.com>
2025-11-17 17:04:25 +0100

    Fix incorrect name of Plant data card

--- Assets/App/Scripts/Data/SSO_CardData_Plants.cs
+++ Assets/App/Scripts/Data/SSO_CardData_Plants.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "SSO_CardPlants", menuName = "SSO/Cards/SSO_CardPlants")]
+[CreateAssetMenu(fileName = "SSO_CardData_Plants", menuName = "SSO/Cards/SSO_CardDataPlants")]
 public class SSO_CardPlants : SSO_CardData
 {
     public override void ApplyEffectToNeighbour(Card neighbour)
