ad6f4e7a4fdfcd57710e5797abda286b88667916
RomainUTR <work.romainutr@gmail.com>
2025-11-18 14:11:04 +0100

    AudioController and Prefab for Audio

---
+++ Assets/App/Scripts/Audio/AudioController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour
+{
+    //[Header("Settings")]
+
+    private float musicVolume, sfxVolume = 1f;
+
+    //[Header("References")]
+
+    [SerializeField] AudioMixer audioMixer;
+
+    //[Header("Input")]
+    //[Header("Output")]
+
+    private void Start()
+    {
+        audioMixer.GetFloat("MusicVolume", out musicVolume);
+
+        audioMixer.GetFloat("SFXVolume", out sfxVolume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        float volumeInDB = Mathf.Log10(volume) * 20;
+        Debug.Log("Volume in DB : " + volumeInDB);
+        Debug.Log("Volume : " + volume);
+        if (volume <= 0.0001f)
+        {
+            audioMixer.SetFloat("MusicVolume", -80f);
+            musicVolume = 0f;
+        }
+        else
+        {
+            audioMixer.SetFloat("MusicVolume", volumeInDB);
+            musicVolume = volume;
+        }
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        float volumeInDB = Mathf.Log10(volume) * 20;
+        Debug.Log("Volume in DB : " +  volumeInDB);
+        Debug.Log("Volume : " + volume);
+        if (volume <= 0.0001f)
+        {
+            audioMixer.SetFloat("SFXVolume", -80f);
+            sfxVolume = 0f;
+        }
+        else
+        {
+            audioMixer.SetFloat("SFXVolume", volumeInDB);
+            sfxVolume = volume;
+        }
+    }
+}
