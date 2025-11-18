44dc3ce895fe9c091e35b53ca81c5ce2dae6e442
parchinsamuel <parchin.samuel@gmail.com>
2025-11-18 11:02:10 +0100

    SSO_CardData Hierarchy

--- Assets/App/Scripts/Data/SSO_CardData_Swamp.cs
+++ Assets/App/Scripts/Data/SSO_CardData_Swamp.cs
@@ -3,23 +3,42 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SSO_CardData_Swamp", menuName = "SSO/Cards/SSO_CardData_Swamp")]
 public class SSO_CardData_Swamp : SSO_CardData
 {
-    public override void ApplyEffectToNeighbour(Card neighbour)
+    public override void ApplyEffectToNeighbour(Card card)
     {
-        SSO_CardData data = neighbour.GetData();
+        Card[] neighbours = card.GetNeighbours();
 
-        if (data is SSO_CardData_Swamp) { return; }
-        else if (data is SSO_CardData_Water)
+        int currentPriority = 100;
+        SSO_CardData currentData = card.GetData();
+
+        for (int i = 0; i < neighbours.Length; i++)
         {
-            neighbour.ChangeData(cardsAvailable.Water);
-        } else if (data is SSO_CardData_Minerals)
-        {
-            neighbour.ChangeData(cardsAvailable.Plant);
-        } else if (data is SSO_CardData_Plants)
-        {
-            neighbour.ChangeData(cardsAvailable.Tree);
-        } else if (data is SSO_CardData_Trees)
-        {
-            neighbour.ChangeData(cardsAvailable.Mineral);
+            if (neighbours[i].GetData() is SSO_CardData_Water && currentPriority > 1)
+            {
+                currentPriority = 1;
+                currentData = neighbours[i].GetData();
+            }
+            else if (neighbours[i].GetData() is SSO_CardData_Plants && currentPriority > 2)
+            {
+                currentPriority = 2;
+                currentData = neighbours[i].GetData();
+            }
+            else if (neighbours[i].GetData() is SSO_CardData_Minerals && currentPriority > 3)
+            {
+                currentPriority = 3;
+                currentData = neighbours[i].GetData();
+            }
+            else if (neighbours[i].GetData() is SSO_CardData_Trees && currentPriority > 4)
+            {
+                currentPriority = 4;
+                currentData = neighbours[i].GetData();
+            }
+            else if (neighbours[i].GetData() is SSO_CardData_Swamp && currentPriority > 5)
+            {
+                currentPriority = 5;
+                currentData = neighbours[i].GetData();
+            }
         }
+
+        card.ChangeData(currentData);
     }
 }
--- Assets/App/Scripts/Data/SSO_CardData_Trees.cs
+++ Assets/App/Scripts/Data/SSO_CardData_Trees.cs
@@ -3,23 +3,42 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SSO_CardData_Trees", menuName = "SSO/Cards/SSO_CardData_Trees")]
 public class SSO_CardData_Trees : SSO_CardData
 {
-    public override void ApplyEffectToNeighbour(Card neighbour)
+    public override void ApplyEffectToNeighbour(Card card)
     {
-        SSO_CardData data = neighbour.GetData();
+        Card[] neighbours = card.GetNeighbours();
 
-        if (data is SSO_CardData_Trees) { return; }
-        else if (data is SSO_CardData_Water)
+        int currentPriority = 100;
+        SSO_CardData currentData = card.GetData();
+
+        for (int i = 0; i < neighbours.Length; i++)
         {
-            neighbour.ChangeData(cardsAvailable.Swamp);
-        } else if (data is SSO_CardData_Minerals)
-        {
-            neighbour.ChangeData(cardsAvailable.Swamp);
-        } else if (data is SSO_CardData_Plants)
-        {
-            neighbour.ChangeData(cardsAvailable.Mineral);
-        } else if (data is SSO_CardData_Swamp)
-        {
-            neighbour.ChangeData(cardsAvailable.Mineral);
+            if (neighbours[i].GetData() is SSO_CardData_Plants && currentPriority > 1)
+            {
+                currentPriority = 1;
+                currentData = neighbours[i].GetData();
+            }
+            else if (neighbours[i].GetData() is SSO_CardData_Water && currentPriority > 2)
+            {
+                currentPriority = 2;
+                currentData = neighbours[i].GetData();
+            }
+            else if (neighbours[i].GetData() is SSO_CardData_Trees && currentPriority > 3)
+            {
+                currentPriority = 3;
+                currentData = neighbours[i].GetData();
+            }
+            else if (neighbours[i].GetData() is SSO_CardData_Minerals && currentPriority > 4)
+            {
+                currentPriority = 4;
+                currentData = neighbours[i].GetData();
+            }
+            else if (neighbours[i].GetData() is SSO_CardData_Swamp && currentPriority > 5)
+            {
+                currentPriority = 5;
+                currentData = neighbours[i].GetData();
+            }
         }
+
+        card.ChangeData(currentData);
     }
 }
