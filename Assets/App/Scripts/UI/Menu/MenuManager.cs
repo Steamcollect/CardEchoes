d4381db26f4c9a223a4d23b4dad8d72c89ba1567
RomainUTR <work.romainutr@gmail.com>
2025-11-18 11:26:00 +0100

    PauseMenu

---
+++ Assets/App/Scripts/UI/Menu/MenuManager.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    //[Header("Settings")]
+
+    private bool isPaused = false;
+    //[Header("References")]
+
+    [SerializeField] private GameObject menuPause;
+
+    //[Header("Input")]
+
+    public InputActionReference pauseInput;
+    //[Header("Output")]
+
+
+    private void OnEnable()
+    {
+        pauseInput.action.started += HandlePause;
+    }
+    private void Start()
+    {
+        menuPause.SetActive(false);
+    }
+
+
+    private void HandlePause(InputAction.CallbackContext ctx)
+    {
+        if (isPaused == true)
+        {
+            ResumeGame();
+        } else
+        {
+            PauseGame();
+        }
+    }
+    public void PauseGame()
+    {
+        if (menuPause != null)
+        {
+            menuPause.SetActive(true);
+            Time.timeScale = 0f;
+            isPaused = true;
+        }
+    }
+
+    public void ResumeGame()
+    {
+        if (menuPause != null)
+        {
+            menuPause.SetActive(false);
+            Time.timeScale = 1f;
+            isPaused = false;
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
